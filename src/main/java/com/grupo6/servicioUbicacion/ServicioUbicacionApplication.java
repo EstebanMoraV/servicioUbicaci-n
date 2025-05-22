18982100155e18bc9d12083c535d93e50f76b61a
Esteban <esteban.morauc@gmail.com>
2025-05-22 14:30:46 -0400

    Creación del microservicio Ubicación

---
+++ src/main/java/com/grupo6/servicioUbicacion/ServicioUbicacionApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioUbicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUbicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUbicacionApplication.class, args);
+	}
+
+}
